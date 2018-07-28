af629edd59cab1a200fafc4cb1b9622833723ed9
736659711@qq.com <736659711@qq.com>
2018-07-28 14:39:13 +0800

    scrapy example

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,2 +1,37 @@
-print('wtf')
-print('wwttff')
+# import scrapy
+
+
+# class QuotesSpider(scrapy.Spider):
+#     name = "quotes"
+
+#     def start_requests(self):
+#         urls = [
+#             'http://quotes.toscrape.com/page/1/',
+#             'http://quotes.toscrape.com/page/2/',
+#         ]
+#         for url in urls:
+#             yield scrapy.Request(url=url, callback=self.parser)
+
+#     def parser(self, response):
+#         page = response.url.split("/")[-2]
+#         filename = 'quotes-%s.html' % page
+#         with open(filename, 'wb') as f:
+#             f.write(response.body)
+#         self.log('Saved file %s ' % filename)
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
