3901a3c54080223067114f5b74fa4bfcdc73dcfd
736659711@qq.com <736659711@qq.com>
2018-07-28 21:47:22 +0800

    qweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqweqwe

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -38,7 +38,7 @@ class AuthorSpider(scrapy.Spider):
 
     def parse(self, response):
         # follow links to author pages
-        for href in response.css('.author + a::attr(href)'):
+        for href in response.css('.author + a::attr(href)').extract():
             yield response.follow(href, self.parse_author)
 
         # follow pagination links
@@ -53,4 +53,56 @@ class AuthorSpider(scrapy.Spider):
             'name': extract_with_css('h3.author-title::text'),
             'birthdate': extract_with_css('.author-born-date::text'),
             'bio': extract_with_css('.author-description::text'),
+        }
+
+# 爬取赛氪上的资料
+
+
+class saikrSpider(scrapy.Spider):
+    name = 'saikr_contury'
+    start_urls = ['https://www.saikr.com/vs/0/4/1/']
+
+    # 爬取一页每条的数据
+    def parse(self, response):
+        for detail_url in response.css('div.event4-1-detail-box a::attr(href)').extract():
+            self.log('detail_url: %s' % detail_url)
+            yield scrapy.Request(detail_url, self.parser_detail)
+
+    def parser_detail(self, response):
+        # 工具
+        def extract_with_css(query, response=response):
+            return response.css(query).extract_first().strip()
+
+        l = response.css('span.title-desc::text').extract()
+
+        s = response.css('li.new-event4-1-info-item')
+
+        yield{
+            # 图片地址
+            'image_url': extract_with_css('div.sk-event4-1-detail-banner img::attr(src)'),
+            # 标题
+            'title': extract_with_css('div.sk-event4-1-detail-banner img::attr(alt)'),
+            # 内容
+            # 'content':extract_with_css('div.event4-1-detail-text-box')
+            # 发布者
+            'publisher': extract_with_css('div.event-user-list-box dd.item-desc::text'),
+            #
+            # 观看量
+            'watch': l[0].strip(),
+            # 种类
+            'team_category': l[1].strip(),
+            # 费用
+            'charge': l[2].strip(),
+            # 级别
+            'level': l[3].strip(),
+            # 参赛对象
+            'object': l[4].strip(),
+            # 举办者
+            'organizationer': extract_with_css('div.info-content::text', response=s[0]),
+            # 报名时间
+            'apply_time': extract_with_css('div.info-content::text', response=s[1]),
+            # 开始时间
+            'competitiontime': extract_with_css('div.info-content::text', response=s[2]),
+            # 比赛类别
+            'com_category': extract_with_css('div.info-content span::text', response=s[3]),
         }
