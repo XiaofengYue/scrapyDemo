f5330a658e89776c5b597f76a8cd48a03c7a4802
736659711@qq.com <736659711@qq.com>
2018-07-28 15:41:10 +0800

    an fix

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,23 +1,3 @@
-# import scrapy
-
-
-# class QuotesSpider(scrapy.Spider):
-#     name = "quotes"
-
-#     def start_requests(self):
-#         urls = [
-#             'http://quotes.toscrape.com/page/1/',
-#             'http://quotes.toscrape.com/page/2/',
-#         ]
-#         for url in urls:
-#             yield scrapy.Request(url=url, callback=self.parser)
-
-#     def parser(self, response):
-#         page = response.url.split("/")[-2]
-#         filename = 'quotes-%s.html' % page
-#         with open(filename, 'wb') as f:
-#             f.write(response.body)
-#         self.log('Saved file %s ' % filename)
 import scrapy
 
 
@@ -25,7 +5,6 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
     ]
 
     def parse(self, response):
@@ -35,3 +14,43 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').extract_first(),
                 'tags': quote.css('div.tags a.tag::text').extract(),
             }
+
+        # # Version-1
+        # next_page = response.css('li.next a::attr(href)').extract_first()
+        # if next_page is not None:
+        #     next_page = response.urljoin(next_page)
+        #     yield scrapy.Request(next_page, callback=self.parse)
+
+        # # Version-2
+        # for href in response.css('li.next a::attr(href)'):
+        #   yield response.follow(href, callback=self.parse)
+
+        # Version-3
+        for a in response.css('li.next a'):
+            yield response.follow(a, callback=self.parse)
+
+
+# 蜘蛛会筛选重复的网页
+class AuthorSpider(scrapy.Spider):
+    name = 'author'
+
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        # follow links to author pages
+        for href in response.css('.author + a::attr(href)'):
+            yield response.follow(href, self.parse_author)
+
+        # follow pagination links
+        for href in response.css('li.next a::attr(href)'):
+            yield response.follow(href, self.parse)
+
+    def parse_author(self, response):
+        def extract_with_css(query):
+            return response.css(query).extract_first().strip()
+
+        yield {
+            'name': extract_with_css('h3.author-title::text'),
+            'birthdate': extract_with_css('.author-born-date::text'),
+            'bio': extract_with_css('.author-description::text'),
+        }
