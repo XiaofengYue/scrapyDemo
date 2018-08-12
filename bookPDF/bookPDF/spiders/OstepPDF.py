4a942dd149301f76a10024ec0647bde0ede4b46a
736659711@qq.com <736659711@qq.com>
2018-08-12 19:23:01 +0800

    下载PDF 附加合并PDF （此PDF是machine learcing的书）

--- bookPDF/bookPDF/spiders/OstepPDF.py
+++ bookPDF/bookPDF/spiders/OstepPDF.py
@@ -11,10 +11,18 @@ class PDFSpider(scrapy.Spider):
 
         trs = response.css('tr td')
 
+        # 加入0页
+        item0 = BookpdfItem()
+        item0['title'] = '0'
+        item0['file_urls'] = ['http://pages.cs.wisc.edu/~remzi/OSTEP/preface.pdf']
+        yield item0
         for link in trs:
             title = str(link.css('small::text').extract_first()).strip()
             if title.isdigit():
                 url = 'http://pages.cs.wisc.edu/~remzi/OSTEP/' + link.css('a::attr(href)').extract_first()
+                # 3这里有个小bug 修复即可
+                if title == '3':
+                    url = 'http://pages.cs.wisc.edu/~remzi/OSTEP/dialogue-virtualization.pdf'
                 item = BookpdfItem()
                 item['title'] = title
                 item['file_urls'] = [url]
---
+++ bookPDF/pdfmerge.py
@@ -0,0 +1,30 @@
+# encoding:utf-8
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+
+def mergePdf(inFileList, outFile):
+    '''
+    合并文档
+    :param inFileList: 要合并的文档的 list
+    :param outFile:    合并后的输出文件
+    :return:
+    '''
+    pdfFileWriter = PdfFileWriter()
+    for inFile in inFileList:
+        # 依次循环打开要合并文件
+        pdfReader = PdfFileReader(open(inFile, 'rb'))
+        numPages = pdfReader.getNumPages()
+        for index in range(0, numPages):
+            pageObj = pdfReader.getPage(index)
+            pdfFileWriter.addPage(pageObj)
+
+        # 最后,统一写入到输出文件中
+        pdfFileWriter.write(open(outFile, 'wb'))
+
+
+inFiles = []
+for i in range(52):
+    strpdf = str(i) + '.pdf'
+    inFiles.append(strpdf)
+
+mergePdf(inFiles, 'ostep.pdf')
